7ce9c2b374cd2286f81da88f9e18c0d7b27b2d16
Zhao DAI <zddai@amazon.com>
2020-01-19 02:27:51 +0000

    Remove debug info.

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,19 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { ExtensionContext, workspace, window } from 'vscode';
+import {
+  ExtensionContext,
+  workspace,
+} from 'vscode';
 
 import sortImportsBeforeSavingDocument from './main';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: ExtensionContext) {
-  window.showInformationMessage('TS Import Sorter: activating.');
   const beforeSave = workspace.onWillSaveTextDocument(event =>
     sortImportsBeforeSavingDocument(event),
   );
   context.subscriptions.push(beforeSave);
-  window.showInformationMessage('TS Import Sorter: activated.');
 }
 
 // this method is called when your extension is deactivated
--- src/main/index.ts
+++ src/main/index.ts
@@ -14,7 +14,6 @@ import parseSource from '../parser';
 import sortImports from '../sort';
 
 export default function sortImportsBeforeSavingDocument(event: TextDocumentWillSaveEvent) {
-  window.showInformationMessage('TS Import Sorter starts.');
   const { document } = event;
   if (!isSupported(document)) return;
 
@@ -22,22 +21,14 @@ export default function sortImportsBeforeSavingDocument(event: TextDocumentWillS
   const { fsPath: fileName } = fileUri;
   const sourceText = document.getText();
   try {
-    window.showInformationMessage('TS Import Sorter: loading config');
     const config = loadConfig(fileUri);
     if (isExcluded(fileName, config)) return;
-    window.showInformationMessage('TS Import Sorter: parsing source');
     const { allIds, importNodes, insertLine } = parseSource(sourceText, fileName);
     const { deleteEdits, noFinalNewLine } = getDeleteEdits(importNodes, insertLine);
-
-    window.showInformationMessage(
-      `TS Import Sorter: sorting with config=${JSON.stringify(config)}`,
-    );
     const groups = sortImports(importNodes, allIds, config);
     const insertSource = composeInsertSource(groups, config, noFinalNewLine);
     const edits = getEdits(deleteEdits, insertSource, insertLine.line);
-    window.showInformationMessage('TS Import Sorter: editing source');
     event.waitUntil(edits);
-    window.showInformationMessage('TS Import Sorter: finished');
   } catch (e) {
     window.showErrorMessage(
       `TSImportSorter failed: ${e.message}.
