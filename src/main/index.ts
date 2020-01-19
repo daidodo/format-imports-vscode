ebc3bc9a7d558936dc74ea66642a2cb6b0e72efe
Zhao DAI <zddai@amazon.com>
2020-01-19 00:02:48 +0000

    Add debug info.

--- src/main/index.ts
+++ src/main/index.ts
@@ -7,6 +7,7 @@ import parseSource from '../parser';
 import sortImports from '../sort';
 
 export default function sortImportsBeforeSavingDocument(event: TextDocumentWillSaveEvent) {
+  window.showInformationMessage('TS Import Sorter starts.');
   const { document } = event;
   if (!isSupported(document)) return;
 
@@ -14,13 +15,18 @@ export default function sortImportsBeforeSavingDocument(event: TextDocumentWillS
   const { uri: fileUri } = document;
   const { fsPath: fileName } = fileUri;
   try {
+    window.showInformationMessage('TS Import Sorter: parsing source');
     const { allIds, importNodes, insertLine } = parseSource(sourceText, fileName);
     const { deleteEdits, noFinalNewLine } = getDeleteEdits(importNodes, insertLine);
+    window.showInformationMessage('TS Import Sorter: loading config');
     const config = loadConfig(fileUri);
+    window.showInformationMessage('TS Import Sorter: sorting');
     const groups = sortImports(importNodes, allIds, config);
     const insertSource = composeInsertSource(groups, config, noFinalNewLine);
     const edits = getEdits(deleteEdits, insertSource, insertLine.line);
+    window.showInformationMessage('TS Import Sorter: editing source');
     event.waitUntil(edits);
+    window.showInformationMessage('TS Import Sorter: finished');
   } catch (e) {
     window.showErrorMessage(
       `TSImportSorter failed: ${e.message}.
