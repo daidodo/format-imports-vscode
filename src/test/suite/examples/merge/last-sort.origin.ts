0207cc1db713f68ab60d89f8b878b7786a1dcf0f
Zhao DAI <zddai@amazon.com>
2020-02-09 23:29:25 +0000

    Add tests.

---
+++ src/test/suite/examples/merge/last-sort.origin.ts
@@ -0,0 +1,4 @@
+import {B} from 'a' //comment 2
+import {X,A} from 'a' // comment 1
+
+export {A,B,X}
---
+++ src/test/suite/examples/merge/last-sort.result.ts
@@ -0,0 +1,4 @@
+import { A, X } from 'a'; // comment 1
+import { B } from 'a'; //comment 2
+
+export {A,B,X}
