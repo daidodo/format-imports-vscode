7c4f826e257d47818c993cd4bcbc2120cba000d2
Zhao DAI <zddai@amazon.com>
2020-02-10 12:19:12 +0000

    Add tests.

---
+++ src/test/suite/examples/compose/max-line-length/origin.ts
@@ -0,0 +1,20 @@
+import 'aaaaaaa';   // comment
+import 'aaaaaaaaaaaaaaaaaaaa';
+
+import AAA from 'aaaaaaaaaa';
+import AA from 'aaaaaaaaaaaa';
+import A from 'a';   //comment
+
+import { BBB } from 'bbbbbb';
+import { BB } from 'bbbbbbbb';
+import { B } from 'b';//commnt
+
+import CCC = require('cccc');
+import CC = require('cccccc');
+import C = require('c');//cmmt
+
+import * as DDD from 'ddddd';
+import * as DD from 'ddddddd'; 
+import * as D from 'd'; //cmmt
+
+export {A,AA,AAA,B,BB,BBB,C,CC,CCC,D,DD,DDD}
---
+++ src/test/suite/examples/compose/max-line-length/result.ts
@@ -0,0 +1,27 @@
+import 'aaaaaaa';   // comment
+import 'aaaaaaaaaaaaaaaaaaaa';
+
+import A
+  from 'a';   //comment
+import AAA from 'aaaaaaaaaa';
+import AA
+  from 'aaaaaaaaaaaa';
+import {
+  B,
+} from 'b';//commnt
+import { BBB } from 'bbbbbb';
+import {
+  BB,
+} from 'bbbbbbbb';
+import C =
+  require('c');//cmmt
+import CCC = require('cccc');
+import CC =
+  require('cccccc');
+import * as D
+  from 'd'; //cmmt
+import * as DDD from 'ddddd';
+import * as DD
+  from 'ddddddd';
+
+export {A,AA,AAA,B,BB,BBB,C,CC,CCC,D,DD,DDD}
