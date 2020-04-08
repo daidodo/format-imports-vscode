e03171f52ef5b15ea62c4c9edca3a91e2cb2da50
Zhao DAI <zddai@amazon.com>
2020-04-08 17:04:56 +0100

    Opt merge.

--- src/config/helper.ts
+++ src/config/helper.ts
@@ -1,23 +1,25 @@
 import {
-  ARRAY_KEYS,
   Configuration,
-  OBJECT_KEYS,
+  KEYS_TO_MERGE,
 } from './types';
 
 export function merge(...configs: Configuration[]) {
   return configs.reduce((a, b) => {
-    const arr: Configuration = ARRAY_KEYS.map(k => {
+    const obj: Configuration = KEYS_TO_MERGE.map(k => {
       const e1 = a[k];
       const e2 = b[k];
-      return { [k]: !e1 ? e2 : !e2 ? e1 : [...e1, ...e2] };
-    }).reduce((v1, v2) => ({ ...v1, ...v2 }));
-    const obj: Configuration = OBJECT_KEYS.map(k => {
-      const e1 = a[k];
-      const e2 = b[k];
-      return { [k]: !e1 ? e2 : !e2 ? e1 : { ...e1, ...e2 } };
+      return {
+        [k]: !e1
+          ? e2
+          : !e2
+          ? e1
+          : Array.isArray(e1) && Array.isArray(e2)
+          ? [...e1, ...e2]
+          : { ...e1, ...e2 },
+      };
     }).reduce((v1, v2) => ({ ...v1, ...v2 }));
 
-    return { ...purify(a), ...purify(b), ...purify(arr), ...purify(obj) };
+    return { ...purify(a), ...purify(b), ...purify(obj) };
   });
 }
 
