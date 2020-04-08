9821421f6dedddcfdcf853ea158745a02ac40843
Zhao DAI <zddai@amazon.com>
2020-04-08 18:46:05 +0100

    Check and complete rule.

--- src/sort/Segment.ts
+++ src/sort/Segment.ts
@@ -6,9 +6,9 @@ export default class Segment {
   private rank_: number;
   private compare_: Comparator;
 
-  constructor(id: SortRule[number], rank: number, map: Map<number, Segment>) {
+  constructor(id: SortRule[number], rank: number, p: { map: Map<number, Segment>; mask: number }) {
     this.rank_ = rank;
-    this.compare_ = this.init(id, map);
+    this.compare_ = this.init(id, p);
   }
 
   get compare() {
@@ -19,26 +19,32 @@ export default class Segment {
     return this.rank_;
   }
 
-  private init(id: SortRule[number], map: Map<number, Segment>): Comparator {
+  private init(id: SortRule[number], p: { map: Map<number, Segment>; mask: number }): Comparator {
+    const { map } = p;
     const a = 'a'.charCodeAt(0);
     const z = 'z'.charCodeAt(0);
     const A = 'A'.charCodeAt(0);
     const Z = 'Z'.charCodeAt(0);
     switch (id) {
       case 'az':
+        p.mask |= 0b1;
         this.setMap(map, a, z);
         return (a, b) => a - b;
       case 'AZ':
+        p.mask |= 0b10;
         this.setMap(map, A, Z);
         return (a, b) => a - b;
       case '_':
+        p.mask |= 0b100;
         this.setMap(map, '['.charCodeAt(0), '`'.charCodeAt(0));
         return (a, b) => a - b;
       case 'Aa':
+        p.mask |= 0b11;
         this.setMap(map, A, Z);
         this.setMap(map, a, z);
         return (a, b) => a - b;
       case 'aA':
+        p.mask |= 0b11;
         this.setMap(map, A, Z);
         this.setMap(map, a, z);
         return (i, j) => {
--- src/sort/config.ts
+++ src/sort/config.ts
@@ -19,23 +19,50 @@ export function configForSort(config: Configuration): SortConfig {
 }
 
 function comparatorFromRule(rule: SortRule | undefined): Comparator {
-  if (!rule || !rule.length) return (a, b) => (a ?? '').localeCompare(b ?? '');
   const map = new Map<number, Segment>();
-  rule.forEach((s, i) => new Segment(s, i, map));
-  return (a, b) => {
-    if (!a) return !b ? 0 : -1;
-    if (!b) return 1;
-    let i = 0;
-    for (; i < a.length && i < b.length; ++i) {
-      const n1 = a.charCodeAt(i);
-      const n2 = b.charCodeAt(i);
-      if (n1 === n2) continue;
-      const s1 = map.get(n1);
-      const s2 = map.get(n2);
-      if (!s1 || !s2) return a.charAt(i).localeCompare(b.charAt(i));
-      if (s1 !== s2) return s1.rank - s2.rank;
-      return s1.compare(n1, n2);
+  const p = { map, mask: 0 };
+  rule?.forEach((s, i) => new Segment(s, i, p));
+  return !checkAndComplete(p, rule?.length ?? 0)
+    ? (a, b) => (a ?? '').localeCompare(b ?? '')
+    : (a, b) => {
+        if (!a) return !b ? 0 : -1;
+        if (!b) return 1;
+        let i = 0;
+        for (; i < a.length && i < b.length; ++i) {
+          const n1 = a.charCodeAt(i);
+          const n2 = b.charCodeAt(i);
+          if (n1 === n2) continue;
+          const s1 = map.get(n1);
+          const s2 = map.get(n2);
+          if (!s1 || !s2) return a.charAt(i).localeCompare(b.charAt(i));
+          if (s1 !== s2) return s1.rank - s2.rank;
+          return s1.compare(n1, n2);
+        }
+        return i < a.length ? 1 : i < b.length ? -1 : 0;
+      };
+}
+
+/**
+ * Check and complete the rule if needed.
+ *
+ * @returns false - The rule is a no-op; true - The rule is completed.
+ */
+function checkAndComplete(p: { map: Map<number, Segment>; mask: number }, nextIndex: number) {
+  const m = p.mask & 0b111;
+  // The rule is a no-op, e.g. [], ['az'].
+  if (!m || !(m & (m - 1))) return false;
+  // If the rule is incomplete, append the missing segment.
+  if (m !== 0b111)
+    switch (0b111 - m) {
+      case 0b1:
+        new Segment('az', nextIndex, p);
+        return true;
+      case 0b10:
+        new Segment('AZ', nextIndex, p);
+        return true;
+      case 0b100:
+        new Segment('_', nextIndex, p);
+        return true;
     }
-    return i < a.length ? 1 : i < b.length ? -1 : 0;
-  };
+  return false; // Something is wrong, fall back to default.
 }
