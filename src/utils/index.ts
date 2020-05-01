7b56e3e63c7c864fbc8dc162ed365839e5ac4b8d
Zhao DAI <zddai@amazon.com>
2020-05-01 02:00:51 +0100

    Fix bugs and add unit tests.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -11,28 +11,29 @@ export function assertNonNull<T>(value: T, message?: string): asserts value is N
   }
 }
 
-export function normalizePath(str: string) {
-  const r = path.normalize(str).replace(/\\/g, '/');
+export function normalizePath(str: string | undefined | null) {
+  if (!str) return '';
+  const r = path.normalize(str.replace(/\\/g, '/'));
   return !str.startsWith('.')
     ? r
     : !r.startsWith('.')
     ? './' + r
     : r === '.'
     ? './'
-    : r === '..'
-    ? '../'
+    : r.endsWith('..')
+    ? r + '/'
     : r;
 }
 
 /**
  * Search for `filename` from `path` and up to all its parents.
  */
-export function findFileFromPathAndParents(filename: string, path?: string) {
+export function findFileFromPathAndParents(filename: string | undefined | null, path?: string) {
   if (!filename) return [];
   // Absolute path: /path/to/file or C:\path\to\file
   if (/^(\/|[a-zA-Z]:\\)/.test(filename)) return [filename];
-  assertNonNull(path);
-  const comp = path.split(/\\|\//);
+  if (!path) return [];
+  const comp = path.split(/[\\/]+/);
   if (isRegularFile(path)) comp.pop();
   const results = [];
   for (; comp.length > 0; comp.pop()) {
@@ -40,6 +41,18 @@ export function findFileFromPathAndParents(filename: string, path?: string) {
     if (isRegularFile(n)) results.push(n);
   }
   return results;
+}
+
+/**
+ * Get parent folder for path.
+ */
+export function parentFolder(path: string | undefined | null) {
+  if (!path) return '';
+  const p = path.replace(/\/+/g, '/').replace(/\\+/g, '\\');
+  const i = p.search(/[\\/][^\\/]*$/);
+  if (i < 0) return '';
+  if (i === 0) return /[\\/]$/.test(p) ? '' : p.substr(0, 1);
+  return p.substr(0, i);
 }
 
 /**
