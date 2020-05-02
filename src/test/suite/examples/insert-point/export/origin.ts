fbaa5f1363f3a4271935499752d8b51a3de4ed77
Zhao DAI <zddai@amazon.com>
2020-05-02 19:00:46 +0100

    Add tests.

---
+++ src/test/suite/examples/insert-point/export/origin.ts
@@ -0,0 +1,8 @@
+export {A, B} from 'a'
+
+const C=1
+export {C}
+const D = 2
+export {D}
+
+export {E}from 'a'
---
+++ src/test/suite/examples/insert-point/export/result.ts
@@ -0,0 +1,6 @@
+export { A, B, E } from 'a';
+
+const C=1
+
+const D = 2
+export { C, D };
