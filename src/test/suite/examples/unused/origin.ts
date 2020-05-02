df68516987455b52abbba159e9424aba834c7962
Zhao DAI <zddai@amazon.com>
2020-05-02 16:08:44 +0100

    Test import {} from 'a'.

--- src/test/suite/examples/unused/origin.ts
+++ src/test/suite/examples/unused/origin.ts
@@ -6,5 +6,8 @@ import D = require('d');
 import AA, {BB} from 'e';
 import * as CC from 'f';
 import DD = require('g');
+import {} from 'h'
+import * as EE from 'i'; // comment 1
+import {} from 'j'  // comment 2
 
 export {A,B,C,D};
