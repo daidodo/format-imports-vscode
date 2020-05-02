180c7b0bd398a2a9cc9518fd819e6927f877eba9
Zhao DAI <zddai@amazon.com>
2020-05-02 12:21:14 +0100

    Add max line len tests for no-semi case.

---
+++ src/test/suite/examples/compose/max-line-length/no-semi/origin.ts
@@ -0,0 +1,20 @@
+import 'aaaaaaaa';   // comment
+import 'aaaaaaaaaaaaaaaaaaaaaa';
+
+import AAA from 'aaaaaaaaaaaa';
+import AA from 'aaaaaaaaaaaaaa';
+import A from 'aaa';   //comment
+
+import { BBB } from 'bbbbbbba';
+import { BB } from 'bbbbbbbbba';
+import { B } from 'bba';//commnt
+
+import CCC = require('ccccca');
+import CC = require('ccccccca');
+import C = require('cca');//cmmt
+
+import * as DDD from 'dddddda';
+import * as DD from 'dddddddda'; 
+import * as D from 'dda'; //cmmt
+
+export {A,AA,AAA,B,BB,BBB,C,CC,CCC,D,DD,DDD}
---
+++ src/test/suite/examples/compose/max-line-length/no-semi/result.jsx
@@ -0,0 +1,27 @@
+import 'aaaaaaaa'   // comment
+import 'aaaaaaaaaaaaaaaaaaaaaa'
+
+import A
+  from 'aaa'   //comment
+import AAA from 'aaaaaaaaaaaa'
+import AA
+  from 'aaaaaaaaaaaaaa'
+import {
+  B,
+} from 'bba'//commnt
+import { BBB } from 'bbbbbbba'
+import {
+  BB,
+} from 'bbbbbbbbba'
+import C =
+  require('cca')//cmmt
+import CCC = require('ccccca')
+import CC =
+  require('ccccccca')
+import * as D
+  from 'dda' //cmmt
+import * as DDD from 'dddddda'
+import * as DD
+  from 'dddddddda'
+
+export {A,AA,AAA,B,BB,BBB,C,CC,CCC,D,DD,DDD}
