9b95dcec6c90878cef3fb9620a2b786c71b20808
Zhao DAI <zddai@amazon.com>
2020-05-03 12:46:13 +0100

    Add path norm tests.

---
+++ src/test/suite/examples/path-norm/export.result.ts
@@ -0,0 +1,10 @@
+export { A, B } from './';
+export { C, D } from '../';
+export { E } from './a/c';
+export { F } from './a/c/';
+export { G } from './a/b/c';
+export { H } from './a/b/c/';
+export { I } from 'a/b/c';
+export { J } from 'a/b/c/';
+export { K } from 'a/c';
+export { L } from 'a/c/';
