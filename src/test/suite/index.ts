bdff0aaab234927d667aa9dbe68d13cbd170ad57
Zhao DAI <zddai@amazon.com>
2020-06-22 00:22:08 +0100

    Refactor test runner.

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,41 +1,10 @@
-import glob from 'glob';
-import Mocha from 'mocha';
 import path from 'path';
 
-import setupCoverage from '../coverage';
+import runner from '../runner';
 
-export function run(): Promise<void> {
-  // Create the mocha test
-  const mocha = new Mocha({
-    ui: 'tdd',
-  });
-  mocha.useColors(true);
-
+export function run() {
   const testsRoot = path.resolve(__dirname, '..');
   const packageRoot = path.resolve(testsRoot, '..', '..');
-  setupCoverage(packageRoot);
 
-  return new Promise((c, e) => {
-    glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-      if (err) {
-        return e(err);
-      }
-
-      // Add files to the test suite
-      files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
-
-      try {
-        // Run the mocha test
-        mocha.run(failures => {
-          if (failures > 0) {
-            e(new Error(`${failures} tests failed.`));
-          } else {
-            c();
-          }
-        });
-      } catch (err) {
-        e(err);
-      }
-    });
-  });
+  return runner(testsRoot, packageRoot);
 }
