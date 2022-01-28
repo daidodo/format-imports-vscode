d98a2f089c9a20086522f52e3dd8d9efa93eae03
Zhao DAI <zddai@amazon.com>
2022-01-28 20:49:53 +0000

    Add webpack.

---
+++ webpack.config.ts
@@ -0,0 +1,50 @@
+import path from 'path';
+import { Configuration } from 'webpack';
+
+const config: Configuration = {
+  target: 'node',
+  entry: './src/extension.ts',
+  output: {
+    path: path.resolve(__dirname, 'out'),
+    filename: 'extension.js',
+    libraryTarget: 'commonjs2',
+    devtoolModuleFilenameTemplate: '../[resource-path]',
+  },
+  devtool: 'source-map',
+  externals: {
+    vscode: 'commonjs vscode',
+  },
+  resolve: {
+    mainFields: ['browser', 'module', 'main'],
+    extensions: ['.ts', '.js'],
+    // fallback: {
+    //   fs: false,
+    //   module: false,
+    //   net: false,
+    //   perf_hooks: false,
+    //   cluster: false,
+    //   url: require.resolve('url'),
+    //   path: require.resolve('path-browserify'),
+    //   os: require.resolve('os-browserify/browser'),
+    //   crypto: require.resolve('crypto-browserify'),
+    //   util: require.resolve('util'),
+    //   assert: require.resolve('assert'),
+    //   zlib: require.resolve('browserify-zlib'),
+    //   stream: require.resolve('stream-browserify'),
+    //   constants: require.resolve('constants-browserify'),
+    //   console: require.resolve('console-browserify'),
+    //   buffer: require.resolve('buffer'),
+    // },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+};
+
+export default config;
