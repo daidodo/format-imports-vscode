bb7d09be7c8d7e01b2bd5cbb85283d989d60805e
daidodo <daidodo@gmail.com>
2022-03-31 22:03:59 +0100

    Update deps and enable webpack.

--- webpack.config.ts
+++ webpack.config.ts
@@ -9,37 +9,13 @@ const config: Configuration = {
     path: path.resolve(__dirname, 'out'),
     filename: 'extension.js',
     libraryTarget: 'commonjs2',
-    devtoolModuleFilenameTemplate: '../[resource-path]',
   },
-  devtool: 'source-map',
   externals: {
     vscode: 'commonjs vscode',
-    eslint: 'commonjs eslint',
   },
   resolve: {
     mainFields: ['browser', 'module', 'main'],
     extensions: ['.ts', '.js'],
-    // fallback: {
-    //   fs: false,
-    //   module: false,
-    //   net: false,
-    //   perf_hooks: false,
-    //   cluster: false,
-    //   url: require.resolve('url'),
-    //   path: require.resolve('path-browserify'),
-    //   os: require.resolve('os-browserify/browser'),
-    //   crypto: require.resolve('crypto-browserify'),
-    //   util: require.resolve('util'),
-    //   assert: require.resolve('assert'),
-    //   zlib: require.resolve('browserify-zlib'),
-    //   stream: require.resolve('stream-browserify'),
-    //   constants: require.resolve('constants-browserify'),
-    //   console: require.resolve('console-browserify'),
-    //   buffer: require.resolve('buffer'),
-    // },
-  },
-  node: {
-    __dirname: 'mock',
   },
   module: {
     rules: [
